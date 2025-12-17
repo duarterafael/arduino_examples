50a71a01e636f5a2ff5b1f3ba5d90b5ed058a678
duarterafael <rafaelbatistaduarte@gmail.com>
2025-12-16 21:37:39 -0300

    Blink exemple

--- Challenge_LDR_Traffic_Light/Challenge_LDR_Traffic_Light.ino
+++ Challenge_LDR_Traffic_Light/Challenge_LDR_Traffic_Light.ino
@@ -1,21 +1,60 @@
-const int LDR_PIN = A0;
-const int LED_PIN = 13;
-int LDRValue ;
+#include "DHT.h"
+
+
+const int pino_dht = 9;
+float temperatura;
+float umidade;
+DHT dht(pino_dht, DHT11);
+const int ledPinV = 7;
+const int ledPinR = 6;
+const int ledPinA = 5;
+
+
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(LDR_PIN, INPUT); 
+  pinMode(ledPinV, OUTPUT);
+  pinMode(ledPinR, OUTPUT);
+  pinMode(ledPinA, OUTPUT);
+  dht.begin();
 }
+
 
 void loop() {
-  LDRValue = analogRead(LDR_PIN);
-  Serial.print("LDR : " );
-  Serial.println(LDRValue);
+  temperatura = dht.readTemperature();
+  umidade = dht.readHumidity();
 
-  if (LDRValue > 700) {  
-    digitalWrite(LED_PIN, HIGH);
-  } else {                   
-    digitalWrite(LED_PIN, LOW);  
+
+  if (Serial.available()) {
+    String comando = Serial.readStringUntil('\n');
+    if (comando == "leitura_tempUmid") {
+      float temperatura = dht.readTemperature();
+      float umidade = dht.readHumidity();
+     
+      if (isnan(temperatura) || isnan(umidade)) {
+        Serial.println("Erro ao ler o sensor");
+      } else {
+        Serial.println(temperatura, 2);
+        Serial.println(umidade, 2);
+      }
+      delay(200);
+    }
+
+
+    if (comando == "led_verde_ligando") {
+      digitalWrite(ledPinV, HIGH);
+    } else if (comando == "led_verde_desligando") {
+      digitalWrite(ledPinV, LOW);
+    }
+    if (comando == "led_vermelho_ligando") {
+      digitalWrite(ledPinR, HIGH);
+    } else if (comando == "led_vermelho_desligando") {
+      digitalWrite(ledPinR, LOW);
+    }
+    if (comando == "led_amarelo_ligando") {
+      digitalWrite(ledPinA, HIGH);
+    } else if (comando == "led_amarelo_desligando") {
+      digitalWrite(ledPinA, LOW);
+    }
   }
-  delay(100);            
 }
+
